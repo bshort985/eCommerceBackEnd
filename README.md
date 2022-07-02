0d6263748c00a35d430f1ae3c504a6f3b143915b
bshort985 <97572888+bshort985@users.noreply.github.com>
2022-07-02 13:05:08 -0400

    first commit

--- README.md
+++ README.md
@@ -22,40 +22,7 @@ THEN I am able to successfully create, update, and delete data in my database
 
 Your database should contain the following four models, including the requirements listed for each model:
 
-*Category
--id
-Integer
-Doesn't allow null values
-Set as primary key
-Uses auto increment
-category_name
-String
-Doesn't allow null values
 
-*Product
--id
-Integer
-Doesn't allow null values
-Set as primary key
-Uses auto increment
-
--product_name
-String
-Doesn't allow null values
-price
-Decimal
-Doesn't allow null values
-Validates that the value is a decimal
-
--stock
-Integer
-Doesn't allow null values
-Set a default value of 10
-Validates that the value is numeric
-
--category_id
-Integer
-References the category model's id
 
 *Tag
 -id
--- models/Product.js
+++ models/Product.js
@@ -1,7 +1,7 @@
 // import important parts of sequelize library
 const { Model, DataTypes } = require('sequelize');
 // import our database connection from config.js
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
 
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
@@ -9,8 +9,41 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    // define id column
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+    type: DataTypes.DECIMAL,
+    allowNull: false,
+    validate: {
+      isDecimal: true
+    },
   },
+  stock: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    defaultValue: 10,
+    validate: {
+      isNumeric: true
+    }
+  },
+  category_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: "category",
+      key: "id"
+    }
+  },
+},
   {
     sequelize,
     timestamps: false,
@@ -18,6 +51,7 @@ Product.init(
     underscored: true,
     modelName: 'product',
   }
+
 );
 
 module.exports = Product;
